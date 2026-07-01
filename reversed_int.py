64e42003e4ead9a50fd3a22dc1be5890ec84a1b3
Renzo Takeda <132412066+Vercii@users.noreply.github.com>
2026-07-01 17:02:42 +0800

    Enhance reversed_int.py with explanatory comments
    Added comments to explain the logic of reversing an integer.

--- reversed_int.py
+++ reversed_int.py
@@ -1,9 +1,9 @@
 number = int(input("Please enter an integer: "))
 reverse = 0
 
-while number != 0:
-    digit = number % 10
-    reverse = reverse * 10 + digit
-    number //= 10
+while number != 0: # as long as number isn't 0, the loop continues
+    digit = number % 10 # gets the last digit by dividing it by 10 and getting the remainder
+    reverse = reverse * 10 + digit # multiplies by 10 in order to have an extra "space" for the number that would be added which is digit
+    number //= 10 # divides number per iteration until it reaches 0
 
 print(reverse)
