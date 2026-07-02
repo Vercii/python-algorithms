f6bd07cb9628389f3fad82731a6107990b97b7a1
Renzo Takeda <renzoisaac912@gmail.com>
2026-07-02 22:10:35 +0800

    modified the algorithm for better optimization.

--- 06 - prime_num.py
+++ 06 - prime_num.py
@@ -1,17 +1,21 @@
-number = int(input("Enter a number: "))
+import math
+number = int(input("Enter a number: ")) # user enters a number here
 
-cnt = number - 1 # automatically subtracts 1 from the start to avoid dividing it by itself
 checker = 0
 
-while cnt != 0:
-    digit = number % cnt # this gets the remainder of number divided by cnt
-    cnt -= 1 # subtracts 1 per iteration
-    if digit == 0:
-        checker += 1 # adds 1 to checker everytime the remainder is 0
+# for loop, takes the range of the inserted number + 1
+for divisor in range(2, int(math.sqrt(number)) + 1):
+    digit = number % (divisor)
+    if digit == 0: 
+        checker += 1
+        break # immediately exits once checker becomes 1
 
-# it will eventually hit 1 because it will be divided by 1 on the previous formula above
-if checker > 1:
+# if checker is greater than or equal to 1 it will be considered NOT prime, vice versa
+if checker == 1:
     print("This is NOT a Prime Number.")
+# special cases for 0 and 1 since the algorithm contradicts them
+elif number == 0 or number == 1:
+    print("This is NOT a Prime Number")
 else:
     print("This is a Prime Number!")
 
